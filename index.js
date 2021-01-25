e511bb5a1b71d2badbe66fe574fda983004ee510
dhymazav <77925287+dhymazav@users.noreply.github.com>
2021-01-25 18:45:44 +0800

    index.js

--- index.js
+++ index.js
@@ -62,17 +62,17 @@ let {
 } = require('./database/json/setting.json')
 
 ban = []
-premium = ["628311800241@s.whatsapp.net"]
+premium = ["6285796582392@s.whatsapp.net"]
 const vcard = 'BEGIN:VCARD\n'
             + 'VERSION:3.0\n'
             + 'FN:Farhan\n'
             + 'ORG:Creator FXC7;\n'
-            + 'TEL;type=CELL;type=VOICE;waid=628311800241:+62 831-1800-241\n'
+            + 'TEL;type=CELL;type=VOICE;waid=6285796582392:+62 857-9658-2392\n'
             + 'END:VCARD'
 
 prefix = '.'
-ig = 'https://www.instagram.com/_farhan_xcode7'
-name = '~ F X C 7 B O T'
+ig = 'https://www.instagram.com/offdhymaz'
+name = '~ X - A V'
 
 function kyun(seconds){
   function pad(s){
@@ -159,9 +159,9 @@ async function starts() {
 			const botFebb = insom ? mek.participant : mek.key.remoteJid
 			pushname2 = client.contacts[botFebb] != undefined ? client.contacts[botFebb].vname || client.contacts[botFebb].notify : undefined
 
-			const BarBarApi = 'barbar api' 
+			const BarBarApi = 'Nkwp9oR7Eg9yyKbejBpm' 
 			const ZeksApi = 'zeks api' 
-			const VhtearApi = 'vhtear api'
+			const VhtearApi = 'K2021Chikanot'
 			const TobzApi = 'BotWeA' 
 			const totalchat = await client.chats.all()
 			
@@ -200,7 +200,7 @@ async function starts() {
 			}
 
 			const botNumber = client.user.jid
-			const ownerNumber = ["628311800241@s.whatsapp.net"]
+			const ownerNumber = ["6285796582392@s.whatsapp.net"]
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
